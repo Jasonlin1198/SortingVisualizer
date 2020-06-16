5d37a933265044b95a3a9c942a2b234185e2d7f8
Jason Lin <jasonlin1198@gmail.com>
2020-06-16 13:26:03 -0700

    included sorting algs

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
-console.log('hello')
+import { bubbleSort, insertionSort, mergeSort } from './sortingAlgorithms.js'
 
+// import html elements
+let inputText = document.getElementById('arrayInput')
+let inputResult = document.getElementById('displayInput')
+let inputButton = document.getElementById('submit')
+let bubbleSortDiv = document.getElementById('bubbleSort')
+let insertionSortDiv = document.getElementById('insertionSort')
+let mergeSortDiv = document.getElementById('mergeSort')
+
+// Initilze Array
+let ARRAY = []
+
+/**
+ *  OnClick for input array button
+ */
+function inputClick() {
+	// Clear result content
+	inputResult.textContent = ''
+	bubbleSortDiv.textContent = 'Bubble Sort: '
+	insertionSortDiv.textContent = 'Insertion Sort: '
+	mergeSortDiv.textContent = 'Merge Sort: '
+
+	// Remove non-numberic characters from input
+	let filteredArray = inputText.value.split('').filter((char) => {
+		return char !== ',' ? true : false
+	})
+	// Append parsed input array
+	filteredArray.map((element) => {
+		inputResult.textContent += element + ' '
+	})
+	// Initialize global array
+	ARRAY = filteredArray
+
+	// Sorting Algorithms
+	let bubbleSorted = bubbleSort(ARRAY)
+	let insertionSorted = insertionSort(ARRAY)
+	let mergeSorted = mergeSort(ARRAY)
+
+	// Display sorted arrays in proper divs
+	bubbleSorted.map((element) => {
+		bubbleSortDiv.textContent += element + ' '
+	})
+	insertionSorted.map((element) => {
+		insertionSortDiv.textContent += element + ' '
+	})
+	mergeSorted.map((element) => {
+		mergeSortDiv.textContent += element + ' '
+	})
+}
+
+// Web page loaded functionality
+addEventListener('DOMContentLoaded', () => {
+	inputButton.addEventListener('click', inputClick)
+})
---
+++ sortingAlgorithms.js
@@ -0,0 +1,75 @@
+/**
+ * Optimal bubbleSorting Algorithm
+ * @param {*} inputArr
+ */
+let bubbleSort = (inputArr) => {
+  let len = inputArr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (inputArr[j] > inputArr[j + 1]) {
+        let tmp = inputArr[j];
+        inputArr[j] = inputArr[j + 1];
+        inputArr[j + 1] = tmp;
+      }
+    }
+  }
+  return inputArr;
+};
+
+/**
+ * Optimal insertionSorting Algorithm
+ * @param {*} inputArr
+ */
+let insertionSort = (inputArr) => {
+  let length = inputArr.length;
+  for (let i = 1; i < length; i++) {
+    let key = inputArr[i];
+    let j = i - 1;
+    while (j >= 0 && inputArr[j] > key) {
+      inputArr[j + 1] = inputArr[j];
+      j = j - 1;
+    }
+    inputArr[j + 1] = key;
+  }
+  return inputArr;
+};
+
+/**
+ * Optimal mergeSorting Algorithms
+ * @param {*} arr
+ */
+let mergeSort = (arr) => {
+  var sorted = arr.slice(),
+    n = sorted.length,
+    buffer = new Array(n);
+
+  for (var size = 1; size < n; size *= 2) {
+    for (var leftStart = 0; leftStart < n; leftStart += 2 * size) {
+      var left = leftStart,
+        right = Math.min(left + size, n),
+        leftLimit = right,
+        rightLimit = Math.min(right + size, n),
+        i = left;
+      while (left < leftLimit && right < rightLimit) {
+        if (sorted[left] <= sorted[right]) {
+          buffer[i++] = sorted[left++];
+        } else {
+          buffer[i++] = sorted[right++];
+        }
+      }
+      while (left < leftLimit) {
+        buffer[i++] = sorted[left++];
+      }
+      while (right < rightLimit) {
+        buffer[i++] = sorted[right++];
+      }
+    }
+    var temp = sorted,
+      sorted = buffer,
+      buffer = temp;
+  }
+
+  return sorted;
+};
+
+export { bubbleSort, insertionSort, mergeSort };
